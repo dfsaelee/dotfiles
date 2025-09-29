36b5aec77843cbe2565b3ae10ee110ae807db1d1
dfsaelee <dsaltac@gmail.com>
2025-09-29 15:21:18 -0700

    install.sh added more installations

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Updating system..."
+# update 
 sudo apt update && sudo apt upgrade -y
 
+# install nvim
 echo "Installing packages..."
 sudo apt install -y neovim git curl build-essential
 
-echo "Linking config files..."
-# Create config dir if missing
+# install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh already installed."
+fi
+
+# set Zsh as default 
+chsh -s "$(which zsh)" || echo "shell not found"
+
+# link config 
 mkdir -p ~/.config
 
-# Link nvim config
 ln -sf "$(pwd)/.config/nvim" ~/.config/nvim
 
 echo "Done!"
